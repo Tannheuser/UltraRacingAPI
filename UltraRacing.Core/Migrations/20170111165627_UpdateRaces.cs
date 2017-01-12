9796aba5a95965576983e088a09f828ed43e0917
Tannheuser <aleksey.ryabinin@gmail.com>
2017-01-12 12:35:38 +0300

    Add second migration

---
+++ UltraRacing.Core/Migrations/20170111165627_UpdateRaces.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UltraRacing.Core.Migrations
+{
+    public partial class UpdateRaces : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Races",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Races",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Races");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Races");
+        }
+    }
+}
